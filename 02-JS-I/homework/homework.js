dd9cde694a91ea0fc065f07203dbeea321ca6e5a
BrianFrow <99160344+BrianFrow@users.noreply.github.com>
2022-02-20 02:23:35 -0300

    cambios en homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -57,14 +57,15 @@ function multiplica(x, y) {
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  var divide = x 
+  var divide = x / y;
+  return divide;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  var 
 }
 
 function tienenMismaLongitud(str1, str2) {
